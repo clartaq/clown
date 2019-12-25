a22fb4033a7bcd0ae6514b4f03f94083f489279b
clartaq <clartaq@axaluna.com>
2019-12-25 13:36:52 -0500

    Added single button dialog tempating and a few error dialogs.

--- src/clown/client/util/dialogs.cljs
+++ src/clown/client/util/dialogs.cljs
@@ -14,31 +14,45 @@
   []
   (toggle-modal "about-to-update-mru-modal"))
 
+(defn enumerate-message-paragraphs
+  "Return a div containing each element in the array of text as paragraphs."
+  [ta]
+  (into [:div] (mapv #(into [:p.dialog-message %]) ta)))    ;)
+
+(defn one-button-dialog-template
+  "Return a piece of hiccup for a one button dialog with the given features."
+  [settings]
+  ;(println "one-button-dialog-template")
+  [:div {:class "modal closed" :id (str (:id-basis settings) "-modal")
+         :role  "dialog"}
+   ;(println "in the hiccup")
+   [:header {:class "modal-header"}
+    [:section {:class "modal-header-left"} (:header settings)]
+    [:section {:class "modal-header-right"}
+     [:input {:type     "button"
+              :class    "tree-demo--button"
+              :id       (str "close- " (:id-basis settings) "-button")
+              :value    "Close"
+              :title    (:button-help settings)
+              :on-click #((:toggle-fn settings))}]]]
+   [:section {:class "modal-guts"} (enumerate-message-paragraphs (:msg-text settings))]
+   [:div {:class "modal-footer"}
+    [:section {:class "tree-demo--button-area"}
+     [:input {:type     "button"
+              :class    "tree-demo--button button-bar-item"
+              :value    (:button-text settings)
+              :title    (:button-help settings)
+              :on-click #((:toggle-fn settings))}]]]])      ;)
+
 (defn layout-about-to-update-mru-modal
   []
-  (let [msg [:div
-             [:p {:class "dialog-message"}
-              "About the update the MRU list in the user preferences."]
-             [:p {:class "dialog-message"}
-              "Return to the editor and change the title before saving."]]]
-    [:div {:class "modal closed" :id "about-to-update-mru-modal" :role "dialog"}
-     [:header {:class "modal-header"}
-      [:section {:class "modal-header-left"} "About to Update"]
-      [:section {:class "modal-header-right"}
-       [:input {:type     "button"
-                :class    "tree-demo--button"
-                :id       "close-about-to-update-button"
-                :value    "Close"
-                :title    "Close this dialog and return to the outliner."
-                :on-click #(toggle-about-to-update-mru-modal)}]]]
-     [:section {:class "modal-guts"} msg]
-     [:div {:class "modal-footer"}
-      [:section {:class "tree-demo--button-area"}
-       [:input {:type     "button"
-                :class    "tree-demo--button button-bar-item"
-                :value    "Ok. Return to the Outliner."
-                :title    "Close this dialog and return to the outliner"
-                :on-click #(toggle-about-to-update-mru-modal)}]]]]))
+  [one-button-dialog-template {:id-basis    "about-to-update-mru"
+                               :header      "About to Update"
+                               :msg-text    ["About the update the MRU list in the user preferences."
+                                             "Return to the editor and change the title before saving."]
+                               :button-text "Ok. Return to the Outliner."
+                               :button-help "Close this dialog and return to the outliner."
+                               :toggle-fn   toggle-about-to-update-mru-modal}])
 
 (defn toggle-bad-edn-modal
   []
@@ -46,29 +60,13 @@
 
 (defn layout-bad-edn-modal
   []
-  (let [msg [:div
-             [:p {:class "dialog-message"}
-              "The file loaded does not appear to contain valid EDN format data."]
-             [:p {:class "dialog-message"}
-              "Try opening another file with the EDN extension."]]]
-    [:div {:class "modal closed" :id "bad-edn-modal" :role "dialog"}
-     [:header {:class "modal-header"}
-      [:section {:class "modal-header-left"} "Sorry! Bad EDN."]
-      [:section {:class "modal-header-right"}
-       [:input {:type     "button"
-                :class    "tree-demo--button"
-                :id       "close-bad-edn-button"
-                :value    "Close"
-                :title    "Close this dialog and return to the outliner."
-                :on-click #(toggle-bad-edn-modal)}]]]
-     [:section {:class "modal-guts"} msg]
-     [:div {:class "modal-footer"}
-      [:section {:class "tree-demo--button-area"}
-       [:input {:type     "button"
-                :class    "tree-demo--button button-bar-item"
-                :value    "Ok. Return to the Outliner."
-                :title    "Close this dialog and return to the outliner"
-                :on-click #(toggle-bad-edn-modal)}]]]]))
+  [one-button-dialog-template {:id-basis    "bad-edn"
+                               :header      "Sorry! Bad EDN."
+                               :msg-text    ["The file loaded does not appear to contain valid EDN format data."
+                                             "Try opening another file with the EDN extension."]
+                               :button-text "Ok. Return to the Outliner."
+                               :button-help "Close this dialog and return to the outliner."
+                               :toggle-fn   toggle-bad-edn-modal}])
 
 (defn toggle-bad-outline-modal
   []
@@ -76,29 +74,13 @@
 
 (defn layout-bad-outline-modal
   []
-  (let [msg [:div
-             [:p {:class "dialog-message"}
-              "The file does not seem to contain a properly formatted outline."]
-             [:p {:class "dialog-message"}
-              "Try opening another outline."]]]
-    [:div {:class "modal closed" :id "bad-outline-modal" :role "dialog"}
-     [:header {:class "modal-header"}
-      [:section {:class "modal-header-left"} "Sorry! Bad Outline."]
-      [:section {:class "modal-header-right"}
-       [:input {:type     "button"
-                :class    "tree-demo--button"
-                :id       "close-bad-outline-button"
-                :value    "Close"
-                :title    "Close this dialog and return to the outliner."
-                :on-click #(toggle-bad-outline-modal)}]]]
-     [:section {:class "modal-guts"} msg]
-     [:div {:class "modal-footer"}
-      [:section {:class "tree-demo--button-area"}
-       [:input {:type     "button"
-                :class    "tree-demo--button button-bar-item"
-                :value    "Ok. Return to the Outliner."
-                :title    "Close this dialog and return to the outliner"
-                :on-click #(toggle-bad-outline-modal)}]]]]))
+  [one-button-dialog-template {:id-basis    "bad-outline"
+                               :header      "Sorry! Bad Outline."
+                               :msg-text    ["The file does not seem to contain a properly formatted outline."
+                                             "Try opening another outline."]
+                               :button-text "Ok. Return to the Outliner."
+                               :button-help "Close this dialog and return to the outliner."
+                               :toggle-fn   toggle-bad-outline-modal}])
 
 (defn toggle-file-read-error-modal
   []
@@ -106,26 +88,10 @@
 
 (defn layout-file-read-error-dlg
   []
-  (let [msg [:div
-             [:p {:class "dialog-message"}
-              "Some type of error occurred when trying to read the file."]
-             [:p {:class "dialog-message"}
-              "Try loading another file or check if the requested file has been moved or deleted."]]]
-    [:div {:class "modal closed" :id "file-read-error-modal" :role "dialog"}
-     [:header {:class "modal-header"}
-      [:section {:class "modal-header-left"} "Sorry! File Problem."]
-      [:section {:class "modal-header-right"}
-       [:input {:type     "button"
-                :class    "tree-demo--button"
-                :id       "close-file-error-button"
-                :value    "Close"
-                :title    "Close this dialog and return to the outliner."
-                :on-click #(toggle-file-read-error-modal)}]]]
-     [:section {:class "modal-guts"} msg]
-     [:div {:class "modal-footer"}
-      [:section {:class "tree-demo--button-area"}
-       [:input {:type     "button"
-                :class    "tree-demo--button button-bar-item"
-                :value    "Ok. Return to the Outliner."
-                :title    "Close this dialog and return to the outliner"
-                :on-click #(toggle-file-read-error-modal)}]]]]))
+  [one-button-dialog-template {:id-basis    "file-read-error"
+                               :header      "Sorry! File Problem."
+                               :msg-text    ["Some type of error occurred when trying to read the file."
+                                             "Try loading another file or check if the requested file has been moved or deleted."]
+                               :button-text "Ok. Return to the Outliner."
+                               :button-help "Close this dialog and return to the outliner."
+                               :toggle-fn   toggle-file-read-error-modal}])
