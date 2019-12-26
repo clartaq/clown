8f829c446bffb08111034e959b35f472aa5e5d13
clartaq <clartaq@axaluna.com>
2019-12-26 15:57:50 -0500

    A new namespace with functions to return information for an empty outline.

---
+++ src/clown/client/util/empty_outline.cljs
@@ -0,0 +1,26 @@
+(ns clown.client.util.empty-outline
+  (:require [cljs-time.core :as dtc]
+            [cljs-time.format :as dtf]
+            [clown.client.tree-ids :as ti]))
+
+(defn empty-outline-file-name
+  []
+  "Unsaved_Outline_File.edn")
+
+(def clown-formatter (dtf/formatter "d MMM yyyy, h:mm:ssa"))
+
+(defn formatted-time-now
+  "Return the current local time formatted as \"26 Dec 2019, 3:21:45pm\"."
+  []
+  (dtf/unparse clown-formatter (dtc/time-now)))
+
+(defn build-empty-outline
+  [aps]
+  (let [formatted-now (formatted-time-now)]
+    {:version          "0.0.1"
+     :author           (:user @aps)
+     :title            "Unsaved Outline"
+     :created          formatted-now
+     :modified         formatted-now
+     :focused-headline (ti/top-tree-id)
+     :tree             [{:topic "Your outline here"}]}))
